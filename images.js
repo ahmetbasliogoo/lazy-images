143a492f85e2f98b98d17061fe48fb69c6e8d467
Ahmet Basli <ahmetbasli@ogoodigital.com>
2021-07-14 13:17:32 +0300

    add dummy image

--- images.js
+++ images.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     for(i = 0; i < 22; i++) {
-        $('#cats').append(`<img class="container__img col-md-6 col-lg-3" data-src="./images/${i+1}.jpg" alt="img">`);
+        $('#cats').append(`<img class="container__img col-md-6 col-lg-3" src="https://dummyimage.com/600x400/d3d3d3/d2f2f2&text=loading"  data-src="./images/${i+1}.jpg" alt="img">`);
       }
 
     $('.container__img').lazy()
